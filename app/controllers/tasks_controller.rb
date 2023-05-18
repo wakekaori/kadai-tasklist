4ad648c60ca343b42200465b348de29aeb2c2ad2
wakekaori <wakekaoridevelopment@gmail.com>
2023-05-18 12:55:06 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクが正常に登録されました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが登録できませんでした"
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクを更新しました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが更新できませんでした"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクが削除されました"
+    redirect_to tasks_url
+  end
+  
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:content)
+end
