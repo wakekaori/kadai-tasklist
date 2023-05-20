890c3af20441e0d0038133f05a94e934c8d38aaa
wakekaori <wakekaoridevelopment@gmail.com>
2023-05-20 04:56:02 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ private
 def set_task
   @task = Task.find(params[:id])
 end
-
+  
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
